bdac2e4a0bb60633d1966f265f01c10aa62d59ad
PolopTech <73751987+PolopTechnology@users.noreply.github.com>
2021-07-06 16:56:00 -0400

    Add files via upload

---
+++ home/home.js
@@ -0,0 +1,13 @@
+// 1 = black cat, 2 = axolotl, 3 = pig;
+
+function image(img) {
+    if(img === 1) {
+        alert("Learn about the history, coolness and cuteness of black cats and why they are NOT bad luck!");
+    }
+    else if(img === 2) {
+        alert("Axolotls are SUPER COOL! They can regenerate and have super derpy faces! But they are going extinct! Why? Learn why and what you can do about it!");
+    }
+    else if(img === 3) {
+        alert("Did you know 121 million pigs are killed each year for food? and Pork is the most eaten meat in the world? But pigs are cute and beautiful creatures! Learn why they are awesome and why you shouldn't eat them!");
+    }
+}
